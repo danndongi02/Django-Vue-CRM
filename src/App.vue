0ba762ee4b4839ac4ed866fcd0cdf040f2449e92
danndongi02 <ianndunguh@gmail.com>
2024-07-04 22:35:54 +0300

    Initialize store

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,21 @@
 
 <script>
 import Navbar from '@/components/Layout/Navbar.vue'
+import axios from 'axios';
+
 export default {
     name: 'App',
     components: {
         Navbar
-    }
+    },
+    beforeCreate() {
+        this.$store.commit('initializeStore');
+
+        if (this.$store.state.token) {
+            axios.defaults.headers.common['Authorization'] = "Token " + this.$store.state.token
+        } else {
+            axios.defaults.headers.common['Authorization'] = ""
+        }
+    },
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,37 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    isLoading: false,
+    isAuthenticated: false,
+    token: "",
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem("token")) {
+        state.token = localStorage.getItem("token");
+        state.isAuthenticated = true;
+      } else {
+        state.token = "";
+        state.isAuthenticated = false;
+      }
+    },
+
+    setLoadingStatus(state, status) {
+      state.isLoading = status;
+    },
+
+    setToken(state, token) {
+      state.token = token;
+      state.isAuthenticated = true;
+    },
+
+    removeToken(state) {
+      state.token = "";
+      state.isAuthenticated = false;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
