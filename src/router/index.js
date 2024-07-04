1dfc08f6562d574e9bfaa70ec5be4d220403932c
danndongi02 <ianndunguh@gmail.com>
2024-07-04 22:42:17 +0300

    Require login for some pages:

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '@/store'
 import HomeView from '../views/HomeView.vue'
 
 import SignUp from '@/views/SignUp.vue'
@@ -25,18 +26,32 @@ const routes = [
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: Dashboard
+    component: Dashboard,
+    meta: {
+      requireLogin: true
+    }
   },
   {
     path: '/dashboard/my-account',
     name: 'MyAccount',
-    component: MyAccount
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
