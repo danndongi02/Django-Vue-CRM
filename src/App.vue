a4a9a533a24b5645bec70dabcecc9be47fbaf3cb
danndongi02 <ianndunguh@gmail.com>
2024-07-04 03:42:23 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Navbar />
+        <div class="text-blue-600 font-bold">Welcome to Ng'aa Maskani Kenya</div>
+
+    </div>
+</template>
+
+<script>
+import Navbar from '@/components/Layout/Navbar.vue'
+export default {
+    name: 'App',
+    components: {
+        Navbar
+    }
+}
+</script>
