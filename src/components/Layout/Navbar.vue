a6d3de9eb9af5cca840bfe0d500d30753ccdb9bf
danndongi02 <ianndunguh@gmail.com>
2024-07-04 04:21:09 +0300

    Create login page

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -5,7 +5,7 @@
 
             <div class="flex gap-x-3 space-x-3 md:order-2 md:space-x-0">
                 <router-link to="/sign-up" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</router-link>
-                <router-link to="/log-in" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</router-link>
+                <router-link to="/login" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</router-link>
             </div>
         </div>
     </nav>
